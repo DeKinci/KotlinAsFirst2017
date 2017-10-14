f2d1a0358e4158ed7e3d8a1170449067a2cc2c18
Nikita Kiselev <knd2000.2@gmail.com>
2017-10-14 22:21:27 +0300

    lesson 5 refactored

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -127,7 +127,7 @@ class Tests {
         assertEquals(listOf(0, 0, 0, 0, 0, 1, 1, 0, 0, 0), computeDeviceCells(10, "+>+>+>+>+", 4))
         assertEquals(listOf(0, 0, -1, -1, -1, 0, 0, 0, 0, 0), computeDeviceCells(10, "<-<-<-<-<-", 6))
         assertEquals(listOf(1, 1, 1, 0, 0, -1, 0, 0, 0, 0), computeDeviceCells(10, "- <<<<< +[>+]", 17))
-        /*assertEquals(listOf(0, 6, 5, 4, 3, 2, 1, 0, -1, -1, -2),
-                computeDeviceCells(11, "<<<<< + >>>>>>>>>> --[<-] >+[>+] >++[--< <[<] >+[>+] >++]", 256))*/
+        assertEquals(listOf(0, 6, 5, 4, 3, 2, 1, 0, -1, -1, -2),
+                computeDeviceCells(11, "<<<<< + >>>>>>>>>> --[<-] >+[>+] >++[--< <[<] >+[>+] >++]", 256))
     }
 }
